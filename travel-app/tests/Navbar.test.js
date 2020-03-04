15bc9a26277064624f4c36d6b8c1da405cdfffde
Harry DB <57152441+HDB-1@users.noreply.github.com>
2020-03-04 12:51:09 +0000

    navbar testing

--- travel-app/tests/Navbar.test.js
+++ travel-app/tests/Navbar.test.js
@@ -12,4 +12,19 @@ describe("shallow Navbar", () => {
     it("should render correctly", () => {
         expect(wrapper).toMatchSnapshot();
     })
+    it("Should render 4 <Link /> element", () => {
+        expect(wrapper.find(".link").length).toEqual(4);
+    })
+    it("Should create a link to the '/' route.", () => {
+        expect(wrapper.find("#home_link").props()).toHaveProperty("href", "/");
+    })
+    it("Should create a link to the '/linestatus' route.", () => {
+        expect(wrapper.find("#line_link").props()).toHaveProperty("href", "/linestatus");
+    })
+    it("Should create a link to the '/map' route.", () => {
+        expect(wrapper.find("#map_link").props()).toHaveProperty("href", "/map");
+    })
+    it("Should create a link to the '/weather' route.", () => {
+        expect(wrapper.find("#weather_link").props()).toHaveProperty("href", "/weather");
+    })
 })
