3a9a47ea453ba55bfd012f8e8fddff830df67813
Harry DB <57152441+HDB-1@users.noreply.github.com>
2020-03-05 16:10:53 +0000

    server.js file add more routes

--- travel-app/server.js
+++ travel-app/server.js
@@ -7,5 +7,8 @@ const port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, "build")));
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
+app.get('/linestatus', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
+app.get('/map', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
+app.get('/weather', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
